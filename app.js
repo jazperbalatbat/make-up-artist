f1991befe64ac39ace7418e0f5baf796e4dac67f
jazper balatbat <jazperbalatbat@gmail.com>
2020-09-09 18:11:37 +0800

    90percent done with about me

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ let about = document.querySelector("#about");
 let indicator = document.querySelectorAll("#design ul li");
 let wcimg = document.querySelectorAll(".wc_img");
 let wciBtn = document.querySelectorAll(".wci_btn");
+let acimg = document.querySelectorAll(".ac_img");
+let aciBtn = document.querySelectorAll(".aci_btn");
 
 //main design structure animation
 function designStructure(...args) {
@@ -37,7 +39,7 @@ function designStructure(...args) {
 function heroPage() {
     // if statement to prevent the js from reloading animation
     if (more.innerHTML != "see more") {
-        designStructure("0%", "5%", "35%", "makeup artist", "10rem", "17%", "50%", "43%", "27%", "1rem", "#333",
+        designStructure("0%", "5%", "35%", "makeup artist", "10rem", "17%", "50%", "43%", "27%", "clamp(.9rem, 1.5vw, 2rem)", "#333",
             "lowercase", "25%", "10%", "", "see more", "auto", "60%", "-150%, 0%", "0%, -150%");
         indi(2);
     }
@@ -47,7 +49,7 @@ function heroPage() {
 function seeMore() {
     // if statement to prevent the js from reloading animation
     if (more.innerHTML != "about me") {
-        designStructure("40%", "95%", "78%", "Art Project", "2.5rem", "0%", "50%", "16%", "10%", "1.5rem", "rgb(206, 206, 206)",
+        designStructure("40%", "95%", "78%", "art projects", "2.5rem", "0%", "50%", "16%", "10%", "clamp(1rem, 2vw, 2rem)", "rgb(206, 206, 206)",
             "capitalize", "14%", "68%", "", "about me", "auto 20% auto auto", "0%", "-50%, 0%", "0%, -150%");
 
         indi(1);
@@ -57,12 +59,16 @@ function seeMore() {
 function aboutMe() {
     // if statement to prevent the js from reloading animation
     if (more != "contacts") {
-        designStructure("0%", "2%", "35%", "The Artist", "2.5rem", "0%", "50%", "16%", "10%", "1.5rem", "rgb(206, 206, 206)",
-            "capitalize", "14%", "10%", ".25rem .5rem 0rem 10rem", "contacts", "auto 20% auto auto", "0%", "-150%, 0%", "0%, 0%");
+        designStructure("0%", "2%", "35%", "the artist", "2.5rem", "0%", "50%", "16%", "10%", "clamp(1rem, 2vw, 2rem)", "rgb(206, 206, 206)",
+            "capitalize", "14%", "10%", ".25rem .0rem 0rem 10rem", "contacts", "auto 20% auto auto", "0%", "-150%, 0%", "0%, 0%");
         indi(2);
     }
 }
 //navigator main button
+if (more.innerHTML == "about me") {
+    aboutMe();
+
+}
 more.addEventListener("click", () => {
     if (more.innerHTML == "see more") {
         seeMore();
@@ -79,7 +85,7 @@ function indi(x) {
     }
     indicator[x].style.cssText = "background:goldenrod";
 }
-
+//indicator main navigation
 indicator[0].addEventListener("click", () => {
     heroPage();
     indi(0);
@@ -106,15 +112,14 @@ let setInterv;
 function hideImg() {
     for (let i = 0; i < 6; i++) {
         wcimg[i].style.cssText = "width: 0%; transition: 300ms ease-in-out;";
-        // console.log(i + ":image width is 0");
     }
 }
+
 
 function autoCarou() {
     setInterv = setInterval(function () {
         hideImg();
         wcimg[pathNum].style.cssText = "width: 100%; transition: 300ms ease-in-out;";
-        // console.log(pathNum + ":image width is 100");
         pathNum++;
         if (pathNum == 6)
             pathNum = 0;
@@ -133,5 +138,39 @@ for (let i = 0; i < 6; i++) {
     });
     wciBtn[i].addEventListener("mouseout", () => {
         autoCarou();
+    });
+}
+//CAROUSEL ABOUT SECTION
+let pathNum2 = 0;
+let setIntervs;
+
+function hideImgs() {
+    for (let i = 0; i < 9; i++) {
+        acimg[i].style.cssText = "width: 0%; transition: 300ms ease-in-out;";
+    }
+}
+
+function carousOver(x) {
+    hideImgs();
+    acimg[x].style.cssText = "width: 100%; transition: 300ms ease-in-out;";
+}
+
+function autoCarous() {
+    setIntervs = setInterval(function () {
+        hideImgs();
+        acimg[pathNum2].style.cssText = "width: 100%; transition: 300ms ease-in-out;";
+        pathNum2++;
+        if (pathNum2 == 9)
+            pathNum2 = 0;
+    }, 2500);
+}
+autoCarous();
+for (let i = 0; i < 9; i++) {
+    aciBtn[i].addEventListener("mouseover", () => {
+        carousOver(i);
+        clearInterval(setIntervs);
+    });
+    aciBtn[i].addEventListener("mouseout", () => {
+        autoCarous();
     });
 }
